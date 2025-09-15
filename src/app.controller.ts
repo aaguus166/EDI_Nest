c7b4f4527db7e9a99f42e3e5513b8c411c2cfa86
aaguss166 <aagusstina.torres@gmail.com>
2025-09-15 19:56:16 -0300

    cambios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('okii')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+@Get('listaUser')
+listaUsuario(): string {
+  return this.appService.getlistaUser();
+}
+
+@Post('usuario')
+crearUsuario(): string {
+  return this.appService.postCrearUsuario();
+}
+
+@Put('actualizar')
+actualizacion(): string {
+  return this.appService.putactualizar();
+}
+
+@Delete('borrarUsuario')
+borrarUsuario(): string {
+  return this.appService.DeleteUsuario();
+}
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,20 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! tengo sueño' ;
   }
+  getlistaUser(): string{
+    return 'esta son la lista de usuario'
+  }
+  postCrearUsuario(): string{
+    return 'usuario creado exitosamete!'
+  }
+  putactualizar(): string{
+    return 'actualizacion del usuario'
+  }
+  DeleteUsuario(): string{
+    return 'borrar lista de usuario'
+  }
+
+  
 }
