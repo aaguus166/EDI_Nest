690ffcb1e32ad0d52c441fba495e970dc4c22ac3
aaguss166 <aagusstina.torres@gmail.com>
2025-09-29 18:47:30 -0300

    nose cambios

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+ usuarios: any[] = [];
+  constructor() {
+   let usuario = {
+    "name": "duki",
+    "surname": "modo diablo",
+    "address":"antezana 247",
+    "phone": "8737898972389"
+   }; 
+    this.usuarios.push(usuario)
+}
+getUsuarios(){
+    return this.usuarios
+}
+
+
+
+}
+
+
